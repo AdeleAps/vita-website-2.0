45391e4d97866439ffb0f67039067dd84b340e63
“AdeleAps” <adelea@inbox.lv>
2023-08-01 21:19:49 +0300

    Minor design adjustments for landing page, new design for footer

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -1,42 +1,62 @@
 import React from "react";
 import styles from "./Footer.module.scss";
-
+import Button from "../reusables/Button/Button";
 const Footer = (props) => {
   return (
     <footer className={styles.footer}>
-      <div>
-        Lai pieteiktos uz konsultācijām, <strong>zvaniet</strong> pa tālruni
-        29549969,{" "}
-        <a href="mailto:vita.psihologs@gmail.com">
-          <strong>rakstiet</strong>
-        </a>{" "}
-        e-pastā vai
-        <a
-          onClick={() => {
-            props.setOpenFormModal(true);
-          }}
-        >
-          <strong> aizpildiet </strong>
-        </a>
-        pieteikumu.
+      <div className={styles.leftSection}>
+        <div>
+          <h3>
+            Piesakieties <br /> konsultācijai
+          </h3>
+          <Button
+            onClick={() => {
+              props.setOpenFormModal(true);
+            }}
+            className="footer"
+          >
+            Pieteikt vizīti
+          </Button>
+        </div>
+        <div className={styles.bottomText}>© Vita Apsīte 2023</div>
       </div>
-      <div>
-        <span> © Vita Apsīte 2023 </span>
-        <a className={styles.email} href="mailto:vita.psihologs@gmail.com">
-          vita.psihologs@gmail.com
-        </a>
+      <div className={styles.middleSection}>
+        <div className={styles.infoOuterWrapper}>
+          <div className={styles.infoWrapper}>
+            <span>Adrese</span>
+            <span>Rīga, Tallinas iela 95, 213. kabinets</span>
+          </div>
+          <div className={styles.infoWrapper}>
+            <span>Telefons</span>
+            <span>+371 29549969</span>
+          </div>
+          <div className={styles.infoWrapper}>
+            <span>E-pasts</span>
+            <span>
+              <a href="mailto:vita.psihologs@gmail.com">
+                vita.psihologs@gmail.com
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className={styles.bottomText}>
+          <span>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/adele-aps%C4%ABte-81693b272/"
+            >
+              Web izstrāde
+            </a>
+          </span>
+          <span>
+            <a target="_blank" href="https://www.linkedin.com/in/montakate">
+              Dizains
+            </a>
+          </span>
+        </div>
       </div>
-      <div className={styles.portfolioLinks}>
-        <span>
-          <a  href="">
-            WEB IZSTRĀDE
-          </a>
-        </span>
-        <span>
-          <a href="">
-            DIZAINS
-          </a>
-        </span>
+      <div className={styles.rightSection}>
+        <img src="/svg/flower.svg" />
       </div>
     </footer>
   );
