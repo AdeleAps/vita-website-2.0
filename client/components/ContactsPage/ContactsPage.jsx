a8aa97a903f4cfb3714e629b3d0a0be9280352ec
Adele Apsite <adele.apsite@accenture.com>
2023-10-03 20:38:30 +0300

    Implement new design for contacts page

--- client/components/ContactsPage/ContactsPage.jsx
+++ client/components/ContactsPage/ContactsPage.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import styles from "./ContactsPage.module.scss";
 import Map from "./Map/Map";
-import MiniPopup from "../reusables/MiniPopup/MiniPopup";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const ContactsPage = (props) => {
-  const [miniPopupOpen, setMiniPopupOpen] = useState(false);
-
   const copyNumber = () => {
     navigator.clipboard.writeText("29549969");
     setMiniPopupOpen(true);
@@ -16,56 +14,51 @@ const ContactsPage = (props) => {
   };
 
   return (
-    <div className={styles.contactsPage}>
-      <div className={styles.info}>
-        <p>
-          Prakses vietas adrese ir{" "}
-          <strong> Rīga, Tallinas iela 95, 213. kabinets</strong>.
-        </p>
-        <p>
-          Orientieri: blakus mājā atradas Rīgas Stradiņa universitātes Sarkanā
-          Krusta medicīnas koledža, gandrīz pretī (ielas pretējā pusē) - Rīgas
-          Kristīgā vidusskola.
-        </p>
-        <p>
-          Pie ēkas ir maksas autostāvvieta, taču, ja auto reģistrē pie manis,
-          tad par to nav jāmaksā.
-        </p>
-        <p>
-          Tuvākais sabiedriskais transports ir: 5., 11., 13., 22. trolejbuss,
-          3., 6., 13., 47., 50., 51. un 52. autobuss.
-        </p>
-        <h4>
-          Lai pieteiktos uz konsultācijām, zvaniet pa tālruni
-          <span className={styles.number} onClick={copyNumber}>
-            {" "}
-            29549969
-            {miniPopupOpen && (
-              <MiniPopup
-                onClick={() => {
-                  closeMiniPopup();
-                }}
-              >
-                Nokopēts
-              </MiniPopup>
-            )}
-          </span>
-          , aizpildiet
-          <button
-            onClick={() => {
-              props.setOpenFormModal(true);
-            }}
-          >
-            pieteikumu
-          </button>
-          vai rakstiet uz{" "}
-          <a className={styles.email} href="mailto:vita.psihologs@gmail.com">
-            vita.psihologs@gmail.com
-          </a>
-          .
-        </h4>
+    <div className={styles.contactsPageContainer}>
+      <div className={styles.contactsPage}>
+        <div className={styles.info}>
+          <h2>Kontakti</h2>
+          <div className={styles.gridContainer}>
+            <div className={styles.gridItem}>Telefons</div>
+            <div className={styles.gridItem}>+371 29659969</div>
+            <div className={styles.gridItem}>E-pasts</div>
+            <div className={styles.gridItem}>
+              <a href="mailto:vita.psihologs@gmail.com">
+                vita.psihologs@gmail.com
+              </a>
+            </div>
+            <div className={styles.gridItem}>Adrese</div>
+            <div className={styles.gridItem}>
+              <p>Rīga, Tallinas iela 95, 213. kabinets</p>
+              <p>
+                Orientieri: blakus mājā atrodas Rīgas Stradiņa universitātes
+                Sarkanā Krusta medicīnas koledža, gandrīz pretī (ielas pretējā
+                pusē) - Rīgas Kristīgā vidusskola.
+              </p>
+            </div>
+            <div className={styles.gridItem}>Kā nokļūt?</div>
+            <div className={styles.gridItem}>
+              <p>Tuvākais sabiedriskais transports ir:</p>
+              <p>
+                5., 11., 13., 22. trolejbuss, 3., 6., 13., 47., 50., 51. un 52.
+                autobuss.
+              </p>
+            </div>
+            <div className={styles.gridItem}>Stāvvieta</div>
+            <div className={styles.gridItem}>
+              Pie ēkas ir maksas autostāvvieta, taču reģistrējoties, par to nav
+              jāmaksā.
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <LazyLoadImage
+            effect="blur"
+            src="/images/contacts-page/contacts-page-hero.png"
+          />
+        </div>
       </div>
-      <div className={styles.mapContainer}>
+      <div className={styles.map}>
         <Map />
       </div>
     </div>
